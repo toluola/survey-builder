e98c7c7639686bcd732afa53f12a32a30041fd27
Tolulope Olaniyan <tolulopeolaniyan@Tolulopes-MacBook-Pro.local>
2020-02-23 23:10:53 +0100

    Home page responsiveness finished

--- src/App.js
+++ src/App.js
@@ -1,16 +1,16 @@
 import React from "react";
 import { BrowserRouter as Router, Route, Switch } from "react-router-dom";
 import HomePage from "./pages/HomePage";
-import NavBar from "./components/NavBar";
+import LoginPage from "./pages/LoginPage";
 import "./App.css";
 
 function App() {
   return (
     <Router>
-      <NavBar />
       <div>
         <Switch>
           <Route exact path="/" component={HomePage} />
+          <Route exact path="/login" component={LoginPage} />
         </Switch>
       </div>
     </Router>
